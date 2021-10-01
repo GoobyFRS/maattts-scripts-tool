fdc8ef7be074a76667f90e0c52c293f2bbb798c2
Maattt GoobyFRS <68407017+GoobyFRS@users.noreply.github.com>
2021-10-01 13:57:07 -0600

    Created caseConvert.py
    Super basic script to convert UPPER case strings into lower case. My usage is for MAC addresses while working with network equipment.

---
+++ caseConvert.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+#Created by Matt Faulkner
+#basic string 'case' converting utility
+blankSpace = "*************************"
+print(blankSpace)
+print("Matts Case Convert Utility")
+print(blankSpace)
+og = str(input("Before: "))
+output = og.lower()
+print("After: " + output)
