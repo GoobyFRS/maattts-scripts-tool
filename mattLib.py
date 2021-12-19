45a202321a6582a8406efc505d8705fdbf5f030b
Maattt GoobyFRS <68407017+GoobyFRS@users.noreply.github.com>
2021-12-19 16:55:34 -0700

    common library file created
    Common library file with functions I might use later.

--- mattLib.py
+++ mattLib.py
@@ -1,6 +1,12 @@
 #!/usr/bin/env python3
 #Library of Functions created by Matt Faulkner, a python beginner.
 #
+#
+import os #linuxPing() 
+
+def main():
+	#
+#
 def caseConverter():
 	xblankspacex = "*************************"
 	ogInput = input("String to Convert: ")
@@ -11,6 +17,14 @@ def caseConverter():
 	elif ccSelect.lower() == 'l' or 'lower':
 		print(ogInput.lower())
 #
-def main():
-        caseConverter()
-main()
+def linuxPing():
+	hostname = input("Target")
+	sendPing = os.system("ping -c 1 " + hostname)
+	if sendPing == 0:
+		print(hostname + " is up.")
+	else:
+		print(hostname + " is down.")
+#
+def windowPing():
+	#
+#
